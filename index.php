369430b297a4258f9d02e8e5de333061fbe80f72
letrongan-dev <57558043+letrongan-dev@users.noreply.github.com>
2019-11-23 13:48:43 +0700

    them login user

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <!-- Title abce aksdjafgukasfiahflkhoo-->
-    <title>One Music - Modern Music HTML5 Template</title>
+    <!-- Title -->
+    <title>Le trong an</title>
 
     <!-- Favicon -->
     <link rel="icon" href="img/core-img/favicon.ico">
